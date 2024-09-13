ec0232812d3dfda9c907a7a85f4ddc3d735f06a6
lleylawa <150817035+lleylawa@users.noreply.github.com>
2024-09-13 13:10:15 +0600

    Create 1.machine epsilon.cpp

---
+++ 1.machine epsilon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    float e  = 1.0; 
+
+    while (e + 1 > 1) {
+        n++;
+        e /= 10;
+    }
+    cout<< n <<endl;
+    return 0;
+}
